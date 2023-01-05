1a14434d9bb65de76d975f238cd4d4b730af6f03
Oliver Njiru <olivernjiru@gmail.com>
2023-01-05 07:59:13 +0300

    add  parentheses

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-
-"""
-
-find magic
-string
-
-"""
 def magic_string(l=[]):
-    """
-
-    store string
-    in the list
-
-    """
     l += ["BestSchool"]
-    return ", ".join(l)
+    return (", ".join(l))
