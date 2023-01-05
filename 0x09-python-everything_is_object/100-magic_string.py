22ca18989586b17ef17e824dc15b7643c2557626
Oliver Njiru <olivernjiru@gmail.com>
2023-01-05 07:57:18 +0300

    betty

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+
+"""
+
+find magic
+string
+
+"""
 def magic_string(l=[]):
+    """
+
+    store string
+    in the list
+
+    """
     l += ["BestSchool"]
     return ", ".join(l)
