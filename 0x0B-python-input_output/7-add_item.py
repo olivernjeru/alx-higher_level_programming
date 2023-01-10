44f29c21f6339e0405bd7b120c5d35a73e3c19f8
Oliver Njiru <olivernjiru@gmail.com>
2023-01-10 18:38:28 +0300

    update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,10 +3,10 @@
 
 from os import path
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json") is false:
+if path.exists("add_item.json") is False:
     save_to_json_file([], "add_item.json")
 my_list = load_from_json_file("add_item.json")
 for i in range(1, len(argv)):
