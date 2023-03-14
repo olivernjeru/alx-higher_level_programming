49c8de992165f8845db46c71f755f6c3eb5b6118
Oliver Njiru <olivernjiru@gmail.com>
2023-03-14 09:51:27 +0300

    beta

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,8 @@
 #!/usr/bin/node
+const args = process.argv
+  .slice(2)
+  .map(arg => Number.parseInt(arg))
+  .sort((a, b) => b - a);
+const val = args.length < 2 ? 0 : args[1];
 
-let biggest = 0;
-let i;
-const arrayNumbers = [];
-
-for (i = 2; i < process.argv.length; i++) {
-  if (Number.isNaN(parseInt(process.argv[i])) === false) {
-    arrayNumbers[i - 2] = parseInt(process.argv[i]);
-  }
-}
-
-if (arrayNumbers.length > 1) {
-  biggest = Math.max.apply(null, arrayNumbers);
-  i = arrayNumbers.indexOf(biggest);
-  arrayNumbers[i] = -Infinity;
-  biggest = Math.max.apply(null, arrayNumbers);
-}
-
-console.log(biggest);
+console.log(val);
