2694069df83f627501d34f2d7a2c6d3399334ebc
Oliver Njiru <olivernjiru@gmail.com>
2023-03-18 17:14:46 +0300

    revert

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = hbtn_0e_0_usa.connect(user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
