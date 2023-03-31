905fa7c45ccb98431a15dac68620ca3e489fdbcd
Oliver Njiru <olivernjiru@gmail.com>
2023-03-31 21:08:27 +0300

    undo fix bug

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,7 +3,8 @@
 Python script that sends a request to the URL and
 displays the value of a variable in the response header
 """
-import requests, sys
+import requests
+import sys
 
 
 if __name__ == "__main__":
